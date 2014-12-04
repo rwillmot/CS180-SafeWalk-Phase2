b44b9f32a24ce485955c5cec2ea9c8d45f61ac7e
Ishaan Bakhle <imbakhle@gmail.com>
2014-12-04 16:29:51 -0500

    Adds spinner listeners

--- SafeWalkAndroidClient/src/edu/purdue/rwillmo/ClientFragment.java
+++ SafeWalkAndroidClient/src/edu/purdue/rwillmo/ClientFragment.java
@@ -9,9 +9,12 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * This fragment is the "page" where the user inputs information about the
@@ -19,19 +22,19 @@ import android.widget.RadioGroup;
  *
  * @author YL
  */
-public class ClientFragment extends Fragment implements OnClickListener,  OnItemSelectedListener{
+public class ClientFragment extends Fragment implements OnClickListener,
+		OnItemSelectedListener {
 
 	/**
 	 * Activity which have to receive callbacks.
 	 */
 	private SubmitCallbackListener activity;
-	
-	private EditText name;	
-	private RadioGroup radioGroup;	
-	private String protocol;	
-	private Button submit;
+
+	private EditText name;
+	private String protocol;
 	private String toLocation;
 	private String fromLocation;
+
 	/**
 	 * Creates a ProfileFragment
 	 * 
@@ -62,40 +65,70 @@ public class ClientFragment extends Fragment implements OnClickListener,  OnItem
 
 		View view = inflater.inflate(R.layout.client_fragment_layout,
 				container, false);
-		
+
 		/**
 		 * Register this fragment to be the OnClickListener for the submit
 		 * Button.
 		 */
 		view.findViewById(R.id.bu_submit).setOnClickListener(this);
-		
+
 		// TODO: import your Views from the layout here. See example in
 		// ServerFragment.
 		this.name = (EditText) view.findViewById(R.id.tv_name);
-	    
-        return view;
+
+		// From spinner
+		Spinner fromSpinner = (Spinner) view.findViewById(R.id.from_spinner);
+		// Create an ArrayAdapter using the string array and a default spinner
+		// layout
+		ArrayAdapter<CharSequence> fromAdapter = ArrayAdapter
+				.createFromResource(view.getContext(), R.array.from_locations,
+						android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		fromAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		fromSpinner.setAdapter(fromAdapter);
+		// Add a listener to the spinner
+		fromSpinner.setOnItemSelectedListener(this);
+
+		// To spinner
+		Spinner toSpinner = (Spinner) view.findViewById(R.id.to_spinner);
+		// Create an ArrayAdapter using the string array and a default spinner
+		// layout
+		ArrayAdapter<CharSequence> toAdapter = ArrayAdapter
+				.createFromResource(view.getContext(), R.array.to_locations,
+						android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		toAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		toSpinner.setAdapter(toAdapter);
+		// Add a listener to the spinner
+		toSpinner.setOnItemSelectedListener(this);
+
+		return view;
 	}
-	
+
 	public String getName() {
 		System.out.println(this.name);
 		return this.name != null ? name.getText().toString() : "Muhammad Lee";
-		
-	}
-	
-	// Listener methods for spinners
-	public void onItemSelected(AdapterView<?> parent, View view, 
-            int pos, long id) {
-        if (id == R.id.from_spinner) {
-        	fromLocation = parent.getItemAtPosition(pos).toString();
-        } else {
-        	toLocation = parent.getItemAtPosition(pos).toString();
-        }
-    }
 
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Nothing Happens
-    }
-	
+	}
+
+	// Listener methods for spinners
+	public void onItemSelected(AdapterView<?> parent, View view, int pos,
+			long id) {
+		if (id == R.id.from_spinner) {
+			fromLocation = parent.getItemAtPosition(pos).toString();
+		} else {
+			toLocation = parent.getItemAtPosition(pos).toString();
+		}
+	}
+
+	public void onNothingSelected(AdapterView<?> parent) {
+		// Nothing Happens
+	}
+
 	/**
 	 * Callback function for the OnClickListener interface.
 	 */
