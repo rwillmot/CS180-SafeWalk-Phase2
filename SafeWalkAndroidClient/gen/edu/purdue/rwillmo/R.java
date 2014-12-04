684a596dd6cb18391974171397687e0b919c4d09
RYAN WILLMOT <rwillmo@purdue.edu>
2014-12-03 22:57:22 -0500

    Fixed hardcoded strings in clientfragment.xml

--- SafeWalkAndroidClient/gen/edu/purdue/rwillmo/R.java
+++ SafeWalkAndroidClient/gen/edu/purdue/rwillmo/R.java
@@ -84,12 +84,19 @@ public final class R {
         public static final int default_command=0x7f060008;
         public static final int default_host=0x7f060006;
         public static final int default_port=0x7f060007;
+        public static final int from=0x7f060010;
         public static final int host=0x7f060009;
         public static final int match=0x7f060004;
+        public static final int name=0x7f06000b;
+        public static final int neutral=0x7f06000e;
         public static final int port=0x7f06000a;
+        public static final int preferences=0x7f06000f;
+        public static final int requester=0x7f06000c;
         public static final int server=0x7f060001;
         public static final int start_over=0x7f060005;
         public static final int submit=0x7f060003;
+        public static final int to=0x7f060011;
+        public static final int volunteer=0x7f06000d;
     }
     public static final class style {
         public static final int ActionBarButton=0x7f070003;
